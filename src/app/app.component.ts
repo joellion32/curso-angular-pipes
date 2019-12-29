3ee971e2d3bed936765dffd20dd1dc3919ba0d3b
Joel <joeltoribiopolanco@gmail.com>
2019-12-28 23:51:01 -0400

    aplicacion terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pipes';
+variable = "Joel";
+nombre = "jOel aGustin toriBio polAnco";
+
+arreglo = [1,2,3,4,5,6,7,8,9,10];
+pi = Math.PI;
+a: number = 0.259;
+salario = 123456;
+
+heroe = {
+nombre: "Logan",
+clave: "Wolverine",
+edad: 500,
+direccion: {
+calle: "primera",
+casa: "19"  
+}  
+}
+
+valorPromesa = new Promise((resolve, reject)=>{
+setTimeout(()=>resolve("Llego la data"), 1000);
+});
+
+
+date = new Date();
+
+// url video
+video:string = "ojwDUImJVWc";
+
+activar:boolean = true;
 }
